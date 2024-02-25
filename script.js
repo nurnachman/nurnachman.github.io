bd2dc5265a178cdee93cb0477fb06229d268a2fe
nur nachman <122021760+nurnachman@users.noreply.github.com>
2024-02-25 12:11:36 +0200

    ios13+ permissions

--- script.js
+++ script.js
@@ -15,6 +15,19 @@ function handleOrientation(event) {
 }
 
 function addOrientationListener() {
-    // Add an event listener for the device orientation event
-    window.addEventListener("deviceorientation", handleOrientation);
+  if (typeof DeviceMotionEvent.requestPermission === 'function') {
+    // Handle iOS 13+ devices.
+    DeviceMotionEvent.requestPermission()
+      .then((state) => {
+        if (state === 'granted') {
+          window.addEventListener('devicemotion', handleOrientation);
+        } else {
+          console.error('Request to access the orientation was rejected');
+        }
+      })
+      .catch(console.error);
+  } else {
+    // Handle regular non iOS 13+ devices.
+    window.addEventListener('devicemotion', handleOrientation);
+  }
 }
