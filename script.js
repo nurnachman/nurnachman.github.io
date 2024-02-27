8ed4b55c7fba86e2de75ea3aa58126c8460638f8
nur nachman <122021760+nurnachman@users.noreply.github.com>
2024-02-27 15:44:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,20 +15,5 @@ function handleOrientation(event) {
 }
 
 function addOrientationListener() {
-  if (typeof DeviceMotionEvent.requestPermission === 'function') {
-    // Handle iOS 13+ devices.
-    DeviceMotionEvent.requestPermission()
-      .then((state) => {
-        if (state === 'granted') {
-          window.addEventListener('devicemotion', handleOrientation);
-        } else {
-          console.error('Request to access the orientation was rejected');
-        }
-      })
-      .catch(console.error);
-  } else {
-    // Handle regular non iOS 13+ devices.
-    window.addEventListener('devicemotion', handleOrientation);
     window.addEventListener('deviceorientation', handleOrientation);
-  }
 }
