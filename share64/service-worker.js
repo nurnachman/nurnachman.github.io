71ea22cf234f37bd250d736b562db83f37222e28
nur nachman <122021760+nurnachman@users.noreply.github.com>
2025-05-31 15:52:40 +0300

    Update service-worker.js

--- share64/service-worker.js
+++ share64/service-worker.js
@@ -1,12 +1,30 @@
+const CACHE_NAME = "share64-cache-v1";
+const urlsToCache = [
+  '/share64/',        // תעדכן לפי הפרויקט שלך
+  '/share64/index.html',
+  '/share64/manifest.json',
+  '/share64/icon-192.png',
+  '/share64/icon-512.png'
+];
+
+// התקנה
 self.addEventListener('install', event => {
-  console.log('Service worker installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
+// אקטיבציה
 self.addEventListener('activate', event => {
-  console.log('Service worker activating...');
+  event.waitUntil(self.clients.claim());
 });
 
+// פניות
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
